a4926a91177109d224d7a0a0534cbd9b3deb6050
Gerard Albanell <gerard@cnsld.cc>
2025-05-14 11:07:21 +0200

    Afegits dotfiles

---
+++ dotfiles/.hyper.js
@@ -0,0 +1,10 @@
+module.exports = {
+	config: {
+		fontSize: 14,
+		fontFamily:
+			'"FiraCode Nerd Font Mono", Menlo, "DejaVuSansM Nerd Font Mono",  Consolas, "Lucida Console", monospace',
+		webGLRenderer: true,
+	},
+	plugins: ["hyper-font-ligatures", "hyperline", "hyper-one-dark"],
+	localPlugins: [],
+};
